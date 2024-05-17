6f29966602d2363ef23853d94d6ef150dc3a3940
Jashanpreet <jashanpreet020105@gmail.com>
2024-05-17 10:19:14 -0500

    made some changes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes, useLocation } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes, useLocation } from 'react-router-dom';
 import { motion, AnimatePresence } from 'framer-motion';
 import logo from './media/logo.png';
 import Profile from './components/User';
